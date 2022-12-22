8d906e60f5de4749b246276c431ae4e63488d912
Batunde-Dev <batunde.dev@gmail.com>
2022-12-22 15:14:58 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_strcmp - compare two string
+*@s1: first str
+*@s2: second str
+*Return: 0
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j = 0;
+
+	while (j == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		j = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+	return (j);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -6,3 +6,5 @@ C - More pointers, arrays and strings
 
 2 - Write a function that copies a string.
 
+3 - Write a function that compares two strings.
+
