ceda9b7b51f0580e3c064436e860b504013f214f
Batunde-Dev <batunde.dev@gmail.com>
2023-03-25 10:16:05 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * _abs - returns absolute value of an int
+  * @n: integer from which to get an absolute value
+  * Return: Always 0
+  */
+int _abs(int n)
+{
+
+	return (n >= 0 ? n : n * -1);
+
+}
