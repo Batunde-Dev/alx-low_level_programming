caf7bb62c650af9e65f8cfd7a4cd204741774acf
Batunde-Dev <batunde.dev@gmail.com>
2023-03-25 14:15:08 -0700

    Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/100-operations.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void lib_add(int a, int b);
+void lib_diff(int a, int b);
+void lib_div(int a, int b);
+void lib_mult(int a, int b);
+
+#endif /* MAIN_H */
