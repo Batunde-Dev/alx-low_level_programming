8334454221a612ed853a3a467c539dae83313978
Batunde-Dev <batunde.dev@gmail.com>
2023-07-12 14:32:59 -0700

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,16 +12,16 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+    if (array == NULL) {
+        return -1;
+    }
 
-	if (array == NULL)
-		return (-1);
+    for (size_t i = 0; i < size; i++) {
+        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+        if (array[i] == value) {
+            return i;
+        }
+    }
 
-	for (i = 0; i < (int)size; i++)
-	{
-		printf("Value checked array[%u] = [%d]\n", i, array[i]);
-		if (value == array[i])
-			return (i);
-	}
-	return (-1);
+    return -1;  // Value not found
 }
